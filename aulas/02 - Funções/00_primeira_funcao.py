ffa0e97d6cf221b3c1bd4274ade019e602eb886a
Amanda Santos <amanda280721@gmail.com>
2025-03-10 10:19:36 -0400

    Primeiras funções

---
+++ aulas/02 - Funções/00_primeira_funcao.py
@@ -0,0 +1,16 @@
+def exibir_mensagem():
+    print("Olá mundo!")
+
+exibir_mensagem()
+
+def exibir_mensagem2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+exibir_mensagem2(nome="Amanda")
+
+
+def exibir_mensagem3(nome="Anônimo"):
+    print(f"Seja bem vindo {nome}!")
+
+exibir_mensagem3()
+exibir_mensagem3(nome="Nicolas")
