304277975d9a8e7bb3633df63a3e9916328b2e10
Amanda Santos <amanda280721@gmail.com>
2025-03-17 23:39:39 -0400

    Desafio Sistema Bancário: Depositar

--- desafios/sistema_bancario.py
+++ desafios/sistema_bancario.py
@@ -1,28 +1,35 @@
 menu = """
-    [1] Depositar
-    [2] Sacar
-    [3] Extrato
-    [0] Sair
+==== Menu ====
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
 
-    => """
+=> """
 
 saldo = 0
 limite = 500
 numero_saques = 0
 LIMITE_SAQUES = 3
-extrato = ""
+extrato = """
+==== Extrato ====
+"""
+
 
 while True:
     opcao = input(menu)
 
     if opcao == "1":
-        print("Depósito")
+        deposito = float(input("Digite o valor do depósito:\n"))
+        saldo += deposito
+        extrato += f"- {deposito}\n"
+        print(saldo)
 
     elif opcao == "2":
         print("Saque")
 
     elif opcao == "3":
-        print("Extrato")
+        print(extrato)
 
     elif opcao == "0":
         break
