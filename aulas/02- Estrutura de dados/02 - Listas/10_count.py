fd25b8e6ca0293d8b7b279eef5cff96a932aa37e
Amanda Santos <amanda280721@gmail.com>
2025-03-28 22:59:30 -0400

    Listas: count

---
+++ aulas/02- Estrutura de dados/02 - Listas/10_count.py
@@ -0,0 +1,7 @@
+# Contar quantas vezes um objeto está presente na lista
+
+cores = ["vermelho", "azul", "verde", "azul"]
+
+print(cores.count("vermelho"))
+print(cores.count("azul"))
+print(cores.count("verde"))
