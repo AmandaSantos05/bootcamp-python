a2d3ad0d29cb55f0c1c4df7644a2e7e5e5ef7bf2
Amanda Santos <amanda280721@gmail.com>
2025-03-28 23:04:35 -0400

    Listas: Extend

---
+++ aulas/02- Estrutura de dados/02 - Listas/11_extend.py
@@ -0,0 +1,10 @@
+# Adiciona os elementos de uma lista a outra lista existente
+# Não elimina valores duplicados
+
+linguagens = ["python", "js", "c"]
+
+print(linguagens)
+
+linguagens.extend(["java", "csharp"])
+
+print(linguagens)
