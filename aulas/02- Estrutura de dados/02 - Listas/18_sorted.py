4dc3a64863f44e66dfd795808f2949b868e558cc
Amanda Santos <amanda280721@gmail.com>
2025-03-31 13:30:49 -0400

    Listas: Sorted

---
+++ aulas/02- Estrutura de dados/02 - Listas/18_sorted.py
@@ -0,0 +1,9 @@
+# Ordenar iteráveis
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(sorted(linguagens, key=lambda x: len(x)))
+
+print(sorted(linguagens, key=lambda x: len(x), reverse=True))
+
+print(sorted(linguagens))
