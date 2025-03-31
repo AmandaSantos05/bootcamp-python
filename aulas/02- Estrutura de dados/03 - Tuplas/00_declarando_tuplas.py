1c6c60d5ee1c83397bedac6318885c372c1284a3
Amanda Santos <amanda280721@gmail.com>
2025-03-31 14:27:25 -0400

    Tuplas: Declarando tuplas

---
+++ aulas/02- Estrutura de dados/03 - Tuplas/00_declarando_tuplas.py
@@ -0,0 +1,15 @@
+# As tuplas são imutáveis
+# Podemos criar tuplas através da classe tuple ou colocando valores separados por vírgula dentro de parênteses
+
+frutas = ("laranja", "pera", "uva",)
+
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
