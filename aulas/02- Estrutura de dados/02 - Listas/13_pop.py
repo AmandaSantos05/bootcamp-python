af2a94f8200825df6c9630b623e502a06bde5f62
Amanda Santos <amanda280721@gmail.com>
2025-03-30 20:56:26 -0400

    Listas: Pop

---
+++ aulas/02- Estrutura de dados/02 - Listas/13_pop.py
@@ -0,0 +1,10 @@
+# Retira o último elemento da lista
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+linguagens.pop() # csharp
+linguagens.pop() # java
+linguagens.pop() # c
+linguagens.pop(0) # python
+
+print(linguagens)
