38909ca4960bebc9f52a427284b12b6d2270fa9d
Amanda Santos <amanda280721@gmail.com>
2025-04-02 15:17:46 -0400

    Conjuntos: Intersection

---
+++ aulas/02- Estrutura de dados/04 - Conjuntos/04_intersection.py
@@ -0,0 +1,7 @@
+# Cria um novo conjunto contendo apenas os elementos que estão em ambos os conjuntos.
+
+conjunto_a = {1, 2, 3}
+conjunto_b = {2, 3, 4}
+
+resultado = conjunto_a.intersection(conjunto_b)
+print(resultado)
