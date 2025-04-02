a4a579fb5ce5a71c226764ecc26a44dd31f05fdb
Amanda Santos <amanda280721@gmail.com>
2025-04-02 15:36:03 -0400

    Correção do código

--- aulas/02- Estrutura de dados/04 - Conjuntos/04_intersection.py
+++ aulas/02- Estrutura de dados/04 - Conjuntos/04_intersection.py
@@ -1,7 +1,11 @@
 # Cria um novo conjunto contendo apenas os elementos que estão em ambos os conjuntos.
+# Use o método intersection() ou o operador &
 
 conjunto_a = {1, 2, 3}
 conjunto_b = {2, 3, 4}
 
 resultado = conjunto_a.intersection(conjunto_b)
 print(resultado)
+
+resultado2 = conjunto_a & conjunto_b
+print(resultado2)
