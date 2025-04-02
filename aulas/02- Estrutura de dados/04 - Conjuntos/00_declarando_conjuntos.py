99ae4348a1acab00ceb1408871a071ad0457441e
Amanda Santos <amanda280721@gmail.com>
2025-04-02 13:51:24 -0400

    Declarando conjuntos

---
+++ aulas/02- Estrutura de dados/04 - Conjuntos/00_declarando_conjuntos.py
@@ -0,0 +1,13 @@
+# Usamos sets para representar conjuntos matemáticos ou eliminar itens duplicados.
+
+numeros = set([1, 2, 3, 1, 3, 4])
+print(numeros)
+
+letras = set("abacaxi")
+print(letras)  
+
+carros = set(("palio", "gol", "celta", "palio"))
+print(carros)
+
+linguagens = {"python", "java", "python"}
+print(linguagens)
