2bb8122864b37979802dda522cc00d068525a4eb
Amanda Santos <amanda280721@gmail.com>
2025-04-10 16:14:56 -0400

    Conjuntos: add

---
+++ aulas/02- Estrutura de dados/04 - Conjuntos/10_add.py
@@ -0,0 +1,13 @@
+# Permite adicionar um único elemento ao conjunto.
+# Se o elemento já estiver presente, o conjunto não será modificado.
+
+sorteio = {1, 23}
+
+sorteio.add(25)
+print(sorteio)
+
+sorteio.add(42)
+print(sorteio)
+
+sorteio.add(25)
+print(sorteio)
