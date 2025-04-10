c0d8e9b4941b1cf876ee673216d62938a52e9001
Amanda Santos <amanda280721@gmail.com>
2025-04-10 10:45:24 -0400

    Conjuntos: issubset

--- aulas/02- Estrutura de dados/04 - Conjuntos/07_issubset.py
+++ aulas/02- Estrutura de dados/04 - Conjuntos/07_issubset.py
@@ -4,9 +4,9 @@
 conjunto_a = {1, 2, 3}
 conjunto_b = {4, 1, 2, 5, 6, 3}
 
-print(2 in conjunto_a)
+print(2 in conjunto_a)  #True
 
-resultado = conjunto_a.issubset(conjunto_b)
+resultado = conjunto_a.issubset(conjunto_b)  # True
 print(resultado)
-resultado = conjunto_b.issubset(conjunto_a)
+resultado = conjunto_b.issubset(conjunto_a)  # False
 print(resultado)
