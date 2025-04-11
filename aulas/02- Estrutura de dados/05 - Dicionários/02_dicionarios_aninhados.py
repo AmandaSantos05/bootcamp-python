eab4c9c96d502e02493ec1dcb472192e98d5b889
Amanda Santos <amanda280721@gmail.com>
2025-04-11 07:22:29 -0400

    Dicionários: Dicionários aninhados

---
+++ aulas/02- Estrutura de dados/05 - Dicionários/02_dicionarios_aninhados.py
@@ -0,0 +1,15 @@
+# Dicionários podem armazenar qualquer tipo de objeto Python como valor,
+# desde que a chave para esse valor seja um objeto imutável (como strings e números).
+
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766", "extra": {"a": 1}},
+}
+
+telefone = contatos["giovanna@gmail.com"]["telefone"]
+print(telefone) # 3443-2121
+
+extra = contatos["melaine@gmail.com"]["extra"]["a"]
+print(extra) # 1
