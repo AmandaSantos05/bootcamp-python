7991a4cda0968bcf582aaaccf4879ed742db46b3
Amanda Santos <amanda280721@gmail.com>
2025-04-10 23:49:07 -0400

    Dicionarios: Declarando dicionarios

---
+++ aulas/02- Estrutura de dados/05 - Dicionários/00_declarando_dicionarios.py
@@ -0,0 +1,11 @@
+# Conjunto não-ordenado de pares chave:valor, 
+# onde as chaves são únicas
+
+pessoa = {"nome": "Guilherme", "idade": 28}
+print(pessoa)
+
+pessoa = dict(nome="Guilherme", idade=28)
+print(pessoa)
+
+pessoa["telefone"] = "3333-3333"  # Adicionando a chave telefone
+print(pessoa)
