ba4b74140977fb5dfc764e5c3b2f6321ecd6b064
Amanda Santos <amanda280721@gmail.com>
2025-04-12 23:20:03 -0400

    Data e hora: date, datetime e time

---
+++ aulas/03 - Data e hora/1_datetime.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime, time
+
+data = date(2024, 4, 12)
+print(data)
+
+print(date.today())
+
+data_hora = datetime(2025, 4, 12, 23, 14, 20)
+print(data_hora)
+print(datetime.today())
+
+hora = time(10,20, 0)
+print(hora)
